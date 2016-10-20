7b8e2fd46042e0dde34d867b29788112204509a0
ian baum and zachary baum <ian@>
2016-10-20 09:16:12 -0400

    create initial angular structure

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="container">
+      <h1>Game Reviews</h1>
+    </div>
+  `
+})
+
+export class AppComponent {
+
+}
